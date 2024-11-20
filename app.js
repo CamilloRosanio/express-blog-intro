c9497f75117ce3d5f86c6f8c9d262a81bf115088
MyUsername <rosanio.c@gmail.com>
2024-11-20 15:29:29 +0100

    recupero progetto iniziale e inizializzazione: npm i + npm i dotenv

--- app.js
+++ app.js
@@ -18,69 +18,53 @@ A questo punto:
 */
 
 
-
+// Dichiaro il REQUIRE del DOTENV
+const dotEnv = require('dotenv').config();
 
 // Dichiarazione dei REQUIRE
 const path = require('path');
-const postArray = require('./main.js');
+const postArray = require('./db-simulation/posts.js');
 
 
 // Dichiarazione EXPRESS JS
 
 const express = require('express');
 const app = express();
-const port = 3000
+const port = process.env.PORT;
+const host = process.env.HOST;
 
-// Dichiarazione degli STATIC ASSETS (PUBLIC)
 app.use(express.static('public'))
 
 
-// Dichiarazione delle ROUTE
+// Dichiarazione del ROUTER
 
 app.get('/', (req, res) => {
   res.send('Server del mio blog')
 })
 
 
-// ROUTE bacheca (senza FILTER)
-// app.get('/bacheca', (req, res) => {
-//     let bachecaArray = {
-//         postNum: postArray.length,
-//         posts: postArray
-//     }
-
-//     res.json(bachecaArray);
-// })
 
 
 // ROUTE bacheca (con FILTER)
 app.get('/bacheca', (req, res) => {
 
-  // Tramite REQ.QUERY richiamo il parametro "term" (nome custom arbitrario) inserito nell'URL e lo dichiaro come variabile
-  // Con l'OPERATOR ?? mi assicuro che SE il valore esiste, uso il valore, altrimenti uso il DEFAULT (in questo caso stringa vuota)
   const term = req.query.term ?? '';
 
   let filteredPosts;
 
   filteredPosts = postArray.filter((post) => {
 
-      // In questo caso sto filtrando per i post in cui nel TITLE è contenuto il TERM (portati entrambi a Lowercase così evito il problema del Case-Sensitive)
       const titleIncludesTerm = post.title.toLowerCase().includes(term.toLowerCase());
 
-      // Ora voglio cercare anche nei TAGS, che è un Array, quindi setto prima la variabile sotto come FALSE
       let tagsIncludesTerm = false;
 
-      // Scorro ciascun TAG per vedere se include il mio TERM
       post.tags.forEach((tag) => {
           if (tag.toLowerCase().includes(term.toLowerCase())) tagsIncludesTerm = true;
       })
 
-      // Il risultato è BOOLEANO, e visto che ho due condizioni le lego con un OR (in questo caso)
       return titleIncludesTerm || tagsIncludesTerm;
   })
 
-  // La RESPONSE sarà l'OBJECT filtrato.
-  // E' bene avere SEMPRE SOLO UNA RESPONSE
   res.json({
     postNum: filteredPosts.length,
     posts: filteredPosts,
@@ -91,5 +75,5 @@ app.get('/bacheca', (req, res) => {
 
 
 app.listen(port, () => {
-  console.log(`Listen attivo e in ascolto sulla PORT ${port}`)
+  console.log(`Listen attivo e in ascolto su ${host}:${port}`)
 })
--- db-simulation/posts.js
+++ db-simulation/posts.js
@@ -1,36 +1,46 @@
-// Creiamo un array dove inserire una lista di almeno 5 post, per ognuno indicare titolo, contenuto, immagine e tags (tags è un array di stringhe)
+// Per comodità e centralizzazione faccio il REQUIRE del .env così da avere i Link delle immagini dinamici
+
+const dotEnv = require('dotenv').config();
+
+const port = process.env.PORT;
+const host = process.env.HOST;
 
 
 
 const postArray = [
     {
+        id: 12,
         title: 'Ciambellone',
         content: 'Delizioso ciambellone',
-        img: '/img/ciambellone.jpeg',
+        img: host + port + `/img/ciambellone.jpeg`,
         tags: ['ciambellone', 'Dolce', 'Torte']
     },
     {
+        id: 21,
         title: 'Cracker di barbabietola',
         content: 'Preparazione cracker di barbabietola',
-        img: '/img/cracker_barbabietola.jpeg',
+        img: host + port + `/img/cracker_barbabietola.jpeg`,
         tags: ['barbabietola', 'Cracker', 'impasto', 'salato']
     },
     {
+        id: 3,
         title: 'Pane fritto',
         content: 'Come fare il pane fritto',
-        img: '/img/pane_fritto_dolce.jpeg',
+        img: host + port + `/img/pane_fritto_dolce.jpeg`,
         tags: ['dolce', 'Pane fritto']
     },
     {
+        id: 5,
         title: 'Pasta di barbabietola',
         content: 'Come preparare la pasta alla barbabietola',
-        img: '/img/pasta_barbabietola.jpeg',
+        img: host + port + `/img/pasta_barbabietola.jpeg`,
         tags: ['barbabietola', 'pasta', 'impasto', 'salato']
     },
     {
+        id: 9,
         title: 'Torta paesana',
         content: '',
-        img: '/img/torta_paesana.jpeg',
+        img: host + port + `/img/torta_paesana.jpeg`,
         tags: ['torte', 'torta paesana', 'dolce']
     }
 ]
